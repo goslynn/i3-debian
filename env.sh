aa96b6a539f015c2a97916c59c68052701c23e18
goslynn <vct.gonzaleza@gmail.com>
2025-12-30 10:01:43 -0300

    agregando sbin al path

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 export PATH="$PATH:/home/vgonzalez/.local/share/JetBrains/Toolbox/scripts"
+export PATH=$PATH:/usr/sbin
 export GTK_THEME=Adwaita:dark
 export GTK_APPLICATION_PREFER_DARK_THEME=1
 export XDG_CURRENT_DESKTOP=i3
